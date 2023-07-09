ce5d9f4332c2f66dcf4b4ca5fc3fa17b62e1678b
Sainan <sainan@calamity.gg>
2023-07-09 17:18:53 +0200

    Add commaCompat test

--- tests/test.lua
+++ tests/test.lua
@@ -14,3 +14,15 @@ do
 	assert(res.section.myFloat == 1.23)
 	assert(res.section.myStr == "Hello, world!")
 end
+
+-- Test commaCompat
+do
+	local res = ini.parse("commaCompat.ini")
+	assert(res.section.myInt == 42)
+	assert(res.section.myFloat == "1,23")
+	assert(res.section.myStr == "Hello, world!")
+	res = ini.parse("commaCompat.ini", { commaCompat = true })
+	assert(res.section.myInt == 42)
+	assert(res.section.myFloat == 1.23)
+	assert(res.section.myStr == "Hello, world!")
+end
