e1352746a5f07147b90c7652385879ba4ac62912
Sainan <sainan@calamity.gg>
2023-07-09 17:00:28 +0200

    Rename tests stuff

--- tests/test.lua
+++ tests/test.lua
@@ -4,11 +4,11 @@ local ini <const> = dofile("../iniparser.lua")
 do
 	os.setlocale("fr-FR.UTF-8")
 
-	local res = ini.parse("basic.ini")
+	local res = ini.parse("usave.ini")
 	assert(res.section.myInt == 42)
 	assert(res.section.myFloat == 1.23)
-	res.save("basic.ini")
-	res = ini.parse("basic.ini")
+	res.save("usave.ini")
+	res = ini.parse("usave.ini")
 	assert(res.section.myInt == 42)
 	assert(res.section.myFloat == 1.23)
 end
